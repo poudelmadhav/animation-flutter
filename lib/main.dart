2aa4d7897ab5dc8b7f741c55f3b6471714a62dde
Madhav Paudel <poudelmadhav143@gmail.com>
2020-04-30 22:25:33 +0545

    gesture detector

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Gesture and Animations',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Gesture and Animations'),
     );
   }
 }
@@ -26,15 +26,71 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int numTaps = 0;
+  int numDoubleTaps = 0;
+  int numLongPress = 0;
+  double posX = 0.0;
+  double posY = 0.0;
+  double boxSize = 150.0;
+
   @override
   Widget build(BuildContext context) {
+    if (posX == 0.0) {
+      center(context);
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(),
+      body: GestureDetector(
+        onTap: () {
+          setState(() {
+            numTaps++;
+          });
+        },
+        onDoubleTap: () {
+          numDoubleTaps++;
+        },
+        onLongPress: () {
+          numLongPress++;
+        },
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+              left: posX,
+              top: posY,
+              child: Container(
+                width: boxSize,
+                height: boxSize,
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: Material(
+        color: Theme.of(context).primaryColorLight,
+        child: Padding(
+          padding: EdgeInsets.all(15.0),
+          child: Text(
+            "Taps: $numTaps - Double Tabs:  $numDoubleTaps, Long Presses: $numLongPress",
+            style: Theme.of(context).textTheme.title,
+          ),
+        ),
       ),
     );
+  }
+
+  void center(BuildContext context) {
+    posX = (MediaQuery.of(context).size.width / 2) - boxSize / 2;
+    posY = (MediaQuery.of(context).size.height / 2) - boxSize / 2 - 30.0;
+
+    setState(() {
+      posX = posX;
+      posY = posY;
+    });
   }
 }
